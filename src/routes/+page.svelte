7edc286816c955830876223f104265eb906b5a3f
matheus.sampaio1 <matheus.sampaio@prodigasistemas.com.br>
2025-03-11 17:36:34 -0300

    [DEV - Projeto] Primeiro Commit - Aula 2

---
+++ src/routes/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    import '../app.css'
+    import Cabecalho from '$components/Cabecalho.svelte';
+    import MinhaLista from '$components/MinhaLista.svelte';
+</script>
+
+<svelte:head>
+    <title>Alura Cook</title>
+</svelte:head>
+
+<div class="container-principal">
+    <Cabecalho />
+
+    <div class="estilo-principal">
+        <div class="minha-lista-container">
+            <MinhaLista />
+        </div>
+    </div>
+</div>
+
+<style>
+    .container-principal {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+    }
+
+    .estilo-principal {
+        text-align: center;
+        padding: 0 5vw 3.375rem;
+        flex: 1;
+    }
+
+    .minha-lista-container {
+        margin-bottom: 2rem;
+    }
+</style>
---
+++ src/routes/receitas/+page.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+    <title>Alura Cook | Receitas</title>
+</svelte:head>
+
+<p>Aqui vão as receitas...</p>
+
+<a href="/">Voltar à Página Inicial</a>
